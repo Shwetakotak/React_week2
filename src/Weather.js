7be15fb3508b733fe8d479043b56dc64ddac4208
Shwetakotak <shwetakotak1@gmail.com>
2022-05-25 17:06:28 -0400

    Update Weather file

--- src/Weather.js
+++ src/Weather.js
@@ -1,9 +1,8 @@
 import React from "react";
-import from "./App.css";
-
+import "./App.css";
 
 export default function Weather() {
-    <>
+  <>
     <form>
       <input type="search" placeholder="Type a city" />
       <input type="submit" value="Search" />
